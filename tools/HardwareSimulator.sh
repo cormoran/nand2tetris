da33600d17e8e35f5ece6265f8fa168e633bf5f0
cormoran <cormoran707@gmail.com>
2020-02-24 23:38:28 +0900

    fix permission

--- tools/HardwareSimulator.sh
+++ tools/HardwareSimulator.sh
@@ -20,7 +20,7 @@ then
 elif [ $# -eq 0 ]
 then
 	# Run hardware simulator in interactive mode
-	java -classpath "${CLASSPATH}:bin/classes:BuiltIn:bin/lib/Hack.jar:bin/lib/HackGUI.jar:bin/lib/Simulators.jar:bin/lib/SimulatorsGUI.jar:bin/lib/Compilers.jar" HardwareSimulatorMain &
+	java -Dsun.java2d.uiScale=2 -classpath "${CLASSPATH}:bin/classes:BuiltIn:bin/lib/Hack.jar:bin/lib/HackGUI.jar:bin/lib/Simulators.jar:bin/lib/SimulatorsGUI.jar:bin/lib/Compilers.jar" HardwareSimulatorMain &
 else
 	# Convert arg1 to an absolute path and run hardware simulator with arg1
 	if [ `echo "$1" | sed -e "s/\(.\).*/\1/"` = / ]
