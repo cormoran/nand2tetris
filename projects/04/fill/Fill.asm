03516d682ac3ef9b0cbda2d49475aceb5c33473c
cormoran <cormoran707@gmail.com>
2020-03-22 13:05:04 +0900

    project4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,50 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    @KBD
+    D = M
+    @WHITE
+    D; JEQ
+(BLACK)
+    @65534
+    D=A
+    @R1
+    M=D
+    @WRITE
+    0; JMP
+(WHITE)
+    @0
+    D=A
+    @R1
+    M=D
+(WRITE)
+    @R0
+    M = 0
+(LOOP1)
+    @R0
+    D = M
+    @SCREEN
+    D = A + D
+    @R2
+    M = D
+    @R1
+    D = M
+    @R2
+    A = M
+    M = D
+    @R0
+    M = M + 1
+    D = M
+    @5
+    D = D - A
+    @LOOP
+    D; JEQ
+    @LOOP1
+    0; JEQ
